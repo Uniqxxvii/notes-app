e6459589d5b9a6ac55db0d922068fa1fa024eb8e
Uniqxxvii <rvtt27@gmail.com>
2025-08-19 10:37:05 -0700

    chore: final version MVP

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Notes App</h1>
+      <h1 className="app-title">Notes App</h1>
       <Header
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
